9211d6246bdf846588a03da5025c5cd3e8ffc06d
rianet <elsajanetmb@gmail.com>
2018-08-23 13:54:23 +0200

    feat(performance): clean up files

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'mws-restaurant-v2';
+const staticCacheName = 'mws-restaurant-v3';
 /**
  * Install service worker and cache assets
  */
@@ -6,35 +6,45 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(staticCacheName)
      .then( cache => {
+       console.log(cache);
       return cache.addAll([
         'index.html',
         'restaurant.html',
+        'js/idb.js',
         'js/main.js',
         'js/restaurant_info.js',
         'js/dbhelper.js',
         'css/styles.css',
-        'css/responsive.css',
-        'data/restaurants.json',
         'img/1-400.jpg',
         'img/1-800.jpg',
+        'img/1.webp',
         'img/2-400.jpg',
         'img/2-800.jpg',
+        'img/2.webp',
         'img/3-400.jpg',
         'img/3-800.jpg',
+        'img/3.webp',
         'img/4-400.jpg',
         'img/4-800.jpg',
+        'img/4.webp',
         'img/5-400.jpg',
         'img/5-800.jpg',
+        'img/5.webp',
         'img/6-400.jpg',
         'img/6-800.jpg',
+        'img/6.webp',
         'img/7-400.jpg',
         'img/7-800.jpg',
+        'img/7.webp',
         'img/8-400.jpg',
         'img/8-800.jpg',
+        'img/8.webp',
         'img/9-400.jpg',
         'img/9-800.jpg',
+        'img/9.webp',
         'img/10-400.jpg',
         'img/10-800.jpg',
+        'img/9.webp',
       ])
       .then(() => self.skipWaiting());
     })
